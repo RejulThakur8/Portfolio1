4c2d7c437d6486682ea43edbd26f9cf5634bf7fc
RejulThakur8 <rejulthakur210@gmail.com>
2025-01-06 01:37:52 +0530

    I have Implement Dark-mode and light mode functionality

--- portfolio/static/js/base.js
+++ portfolio/static/js/base.js
@@ -1,3 +1,14 @@
-let button = document.querySelector(".theme-toggle-btn");
-let body = document.querySelector('body');
-const currentmode = "light";
+const buttons = document.querySelector("#button");
+let body = document.querySelector("body");
+
+buttons.addEventListener("click", () => {
+    if (body.classList.contains("dark-theme")){
+        body.classList.remove("dark-theme");
+        buttons.innerHTML = "Dark Mode";
+    } else {
+        currentmode = "light";
+        body.classList.add("dark-theme");
+        buttons.innerHTML = "Light Mode";
+    }
+    
+});
