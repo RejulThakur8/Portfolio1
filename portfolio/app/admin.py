fc3113cea7c05ded20ba862db86eb5ad5f33bba8
RejulThakur8 <rejulthakur210@gmail.com>
2025-01-15 19:34:22 +0530

    Add My Eduction and Projects & skills

--- portfolio/app/admin.py
+++ portfolio/app/admin.py
@@ -5,4 +5,17 @@ from .models import *
 class ContactUsAdmin(admin.ModelAdmin):
     list_display = ['Name','Email','Phone','Subject','Message']
 
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ['title','school_name','university_name','location','duration']
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title','company','skills','location','duration']
+
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ['title','back']
+
 admin.site.register(Contactus,ContactUsAdmin)
+admin.site.register(Eduction,EducationAdmin)
+admin.site.register(Course,CourseAdmin)
+admin.site.register(Projects,ProjectsAdmin)
+admin.site.register(image)
--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -1,15 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import *
+from .forms import ContactForm
 
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    projects = Projects.objects.all()
+    education = Eduction.objects.all()
+    profile = image.objects.all()
+    if request.method=="POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Your Messages is Submitted Successfully")
+            return redirect('/home/')
+    else:
+        form = ContactForm()
+    return render(request,'index.html',{'education':education,'projects':projects,'form':form,'profile':profile})
 
 def about(request):
-    return render(request,'about.html')
+    profile = image.objects.all()
+    return render(request,'about.html',{'profile':profile})
+
 
 def eduction(request):
-    return render(request,'education.html')
+    education = Eduction.objects.all()
+    course = Course.objects.all()
+    return render(request,'education.html',{'education':education,'course':course})
 
-def project(request):
-    return render(request,'Projects.html')
+def project(request,id):
+    projects = Projects.objects.all()
+    return render(request,'Projects.html',{'projects':projects})
+
+def resume(request):
+    return render(request,'resume.html')
+
+def contact(request):
+    if request.method=="POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Your form is Submitted Successfully")
+            return redirect('/contact/')
+    else:
+        form = ContactForm()
+
+    return render(request,'contact.html' , {'form':form})
